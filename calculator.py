51f23211261d67f400b8ca6f85e0f88a5f768129
achandler09 <achandler3@ufl.edu>
2025-11-13 17:51:22 -0500

    my first commit

--- calculator.py
+++ calculator.py
@@ -5,7 +5,31 @@ calculator.py
 One function per operation, in order.
 """
 # First example
-def add(a, b): 
-    pass
+import math
+
+def add(a, b):
+    return a+b
+
+def subtract(a,b):
+    return a-b
+
+def multiply(a,b):
+    return a*b
+
+def divide(a,b):
+    if a==0:
+        raise ZeroDivisionError
+    return b/a
+
+def logarithm(a,b):
+
+    if b<=0 or a<0 or a==1:
+        raise ValueError
+    if b==math.e:
+        return math.log(a)
+    return math.log(a,b)
+
+def exponent(a,b):
+    return a**b
 
 
